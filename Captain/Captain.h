f508f0a5559081581b097479264cef7fdabb4969
Vincent Esche <regexident@gmail.com>
2018-05-25 19:34:08 +0200

    Initial Commit

---
+++ Captain/Captain.h
@@ -0,0 +1,19 @@
+//
+//  Captain.h
+//  Captain
+//
+//  Created by Vincent Esche on 5/25/18.
+//  Copyright © 2018 Vincent Esche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Captain.
+FOUNDATION_EXPORT double CaptainVersionNumber;
+
+//! Project version string for Captain.
+FOUNDATION_EXPORT const unsigned char CaptainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Captain/PublicHeader.h>
+
+
