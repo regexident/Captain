49231c9945c24c19706463963073d4600b3a498e
Vincent Esche <regexident@gmail.com>
2018-05-26 16:08:40 +0200

    Added demo project

---
+++ CaptainDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CaptainDemo
+//
+//  Created by Vincent Esche on 5/25/18.
+//  Copyright © 2018 Vincent Esche. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
